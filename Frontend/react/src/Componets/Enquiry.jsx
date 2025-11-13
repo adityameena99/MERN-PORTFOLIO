b2fe7edbe826c563fb699d5145e0c9e3af09276a
Aditya Meena <130398523+adityameena99@users.noreply.github.com>
2025-11-13 16:09:18 +0530

    Update Enquiry.jsx

--- Frontend/react/src/Componets/Enquiry.jsx
+++ Frontend/react/src/Componets/Enquiry.jsx
@@ -19,7 +19,7 @@ const Enquiry = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://mern-portfolio-1-il6y.onrender.com/api/contact", {
+      const res = await fetch("https://mern-portfolio-3-gw14.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
