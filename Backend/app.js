e1600145b8555cf8b20e074c6dd484b57ec8464f
Aditya Meena <130398523+adityameena99@users.noreply.github.com>
2025-11-18 16:49:05 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://my-portfolio-899.vercel.app", 
+  origin: "https://mern-portfolio-d64a.vercel.app", 
   credentials: true,
 }));
 
