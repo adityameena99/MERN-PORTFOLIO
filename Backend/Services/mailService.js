b9f5e6a49be1b6dc2a082f7a1f90b258c95f5357
Aditya Meena <130398523+adityameena99@users.noreply.github.com>
2025-11-22 15:19:27 +0530

    Update mailService.js

--- Backend/Services/mailService.js
+++ Backend/Services/mailService.js
@@ -1,12 +1,45 @@
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+
+// const sendMail = async (name, email, message, phone) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: email,
+//     to: process.env.TO_EMAIL,
+//     subject: `📩 New Portfolio Message from ${name}`,
+//     text: `
+// Name: ${name}
+// Email: ${email}
+// Phone: ${phone || "N/A"}
+// Message: ${message}
+//     `,
+//   };
+
+//   console.log("Sending email with:", { name, email, phone, message });
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendMail;
+
+
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const sendMail = async (name, email, message, phone) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS,
     },
   });
 
@@ -28,3 +61,4 @@ Message: ${message}
 };
 
 module.exports = sendMail;
+
